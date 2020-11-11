d8a06b0bba03013beb189b34226b0dbb3e9d71ce
Richard Mižák <richard.mizak@kosickaakademia.sk>
2020-11-11 18:28:14 +0100

    Student.java

--- src/sk/kosickaakademia/mizak/programko/File/CreateAndWrite.java
+++ src/sk/kosickaakademia/mizak/programko/File/CreateAndWrite.java
@@ -1,14 +1,20 @@
 package sk.kosickaakademia.mizak.programko.File;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.FileWriter;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
-public class CreateAndWrite {
+public class CreateAndWrite{
 
-        public static void main(String[] args) {
+
+    public void CreateAndWriteProgram(String fileName) {
             try {
-                File myFile = new File("a.txt");
+                SimpleDateFormat sdf = new SimpleDateFormat("hh-mm-ss");
+                Date curDate = new Date();
+                String strDate = sdf.format(curDate);
+                fileName = strDate+".txt";
+                File myFile = new File(fileName);
                 if (myFile.createNewFile()) {
                     System.out.println("File created: " + myFile.getName());
                 } else {
@@ -18,15 +24,32 @@ public class CreateAndWrite {
                 System.out.println("An error occurred.");
                 e.printStackTrace();
             }try {
-                FileWriter myWriter = new FileWriter("a.txt");
+                FileWriter myWriter = new FileWriter(fileName);
                 myWriter.write("hovno");
+
                 myWriter.close();
                 System.out.println("Successfully wrote to the file.");
             } catch (IOException e) {
                 System.out.println("An error occurred.");
                 e.printStackTrace();
             }
+            try {
+                File myFile = new File(fileName);
+            Scanner myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
-
     }
+}
+
+
+
+
+
 
--- src/sk/kosickaakademia/mizak/programko/File/Main.java
+++ src/sk/kosickaakademia/mizak/programko/File/Main.java
@@ -2,7 +2,8 @@ package sk.kosickaakademia.mizak.programko.File;
 
 public class Main {
     public static void main(String[] args) {
-        FileReader rwFile=new FileReader();
-        rwFile.FileReaderProgram("resource/a.txt");
+        CreateAndWrite cwrFile=new CreateAndWrite();
+        cwrFile.CreateAndWriteProgram("");
+
     }
 }
